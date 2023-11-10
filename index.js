541813ae1465b2a2d6eb719c39788ab4df442086
William Writer <wiwr3415@colorado.edu>
2023-11-09 17:00:20 -0700

    Figured out all of the components to make docker-compose up run properly

--- index.js
+++ index.js
@@ -62,11 +62,14 @@ app.use(
 // *****************************************************
 
 // TODO - Include your API routes here
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
 
 
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
