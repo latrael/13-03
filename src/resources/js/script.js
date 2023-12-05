bb437ffbc5ac09b5457ea392d81b965d4046bcba
Nabil Djaber <nadj7130@colorado.edu>
2023-12-05 11:40:49 -0700

    Events database changed, events show up in the home page calendar

--- src/resources/js/script.js
+++ src/resources/js/script.js
@@ -10,8 +10,9 @@ function initializeContent(){
 
 }
 
-function loadCommunityCalendar(communityName) {
+function loadCommunityCalendar(communityName, eventData) {
   console.log("Community Name:", communityName); // Debugging
+  console.log("Event Data:", eventData); // Debugging
 
   if(calendar) {
     calendar.destroy();
@@ -19,15 +20,20 @@ function loadCommunityCalendar(communityName) {
   var calendarElement = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarElement, {
       initialView: 'dayGridMonth',
-      headerToolbar: {
-        start: 'title', // will normally be on the left. if RTL, will be on the right
-        center: '',
-        end: 'today prev,next' // will normally be on the right. if RTL, will be on the left
-      }
+      eventDidMount: function(info) {
+        console.log("TESTING TOOLTIP: ", info.event.extendedProps.description);
+        var tooltip = new bootstrap.Tooltip(info.el, {
+          title: info.event.title,
+          placement: 'top',
+          trigger: 'hover',
+          container: 'body'
+        });
+      },
+      events: eventData
     });
     var calendarTitle = document.getElementById('calendar-name');
     calendarTitle.innerHTML = communityName;
   // Remove all existing events and rerender the calendar
-  calendar.removeAllEvents();
+  //calendar.removeAllEvents();
   calendar.render();
 }
